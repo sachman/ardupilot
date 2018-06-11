a46abd47fd59ae529ee7503b6d30515b1e44e4b4
sbanshi <suhasbanshiwala@gmail.com>
2018-06-12 01:20:04 +0530

    Camera trig enable for SITL

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -215,6 +215,7 @@ class sitl(Board):
         env.DEFINES.update(
             CONFIG_HAL_BOARD = 'HAL_BOARD_SITL',
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_NONE',
+            CAMERA = 'ENABLED'
         )
 
         if not cfg.env.DEBUG:
